cc2b985175b6c2598fb57269e09bd98bf3dbdc05
dshvaika <dshvaika@thingsboard.io>
2026-07-28 16:20:56 +0300

    test(rpc-node): cover the response deadline fallback on the DTO instead of mocking the scheduler

--- application/src/test/java/org/thingsboard/server/service/rpc/DefaultTbRuleEngineRpcServiceTest.java
+++ application/src/test/java/org/thingsboard/server/service/rpc/DefaultTbRuleEngineRpcServiceTest.java
@@ -17,47 +17,23 @@ package org.thingsboard.server.service.rpc;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
-import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.test.util.ReflectionTestUtils;
-import org.thingsboard.rule.engine.api.RuleEngineDeviceRpcRequest;
 import org.thingsboard.server.cluster.TbClusterService;
 import org.thingsboard.server.common.data.id.DeviceId;
-import org.thingsboard.server.common.data.id.TenantId;
 import org.thingsboard.server.common.data.msg.TbMsgType;
 import org.thingsboard.server.common.msg.TbMsg;
 import org.thingsboard.server.common.msg.TbMsgMetaData;
-import org.thingsboard.server.common.msg.queue.ServiceType;
-import org.thingsboard.server.common.msg.queue.TopicPartitionInfo;
 import org.thingsboard.server.gen.transport.TransportProtos;
-import org.thingsboard.server.queue.discovery.PartitionService;
 
 import java.util.UUID;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
-import java.util.stream.Stream;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.then;
-import static org.mockito.Mockito.mock;
 
 @ExtendWith(MockitoExtension.class)
 class DefaultTbRuleEngineRpcServiceTest {
 
-    private static final TenantId TENANT_ID = TenantId.fromUUID(UUID.fromString("d5a6e5c0-1f68-4b0e-8f9a-2f9b6a7a1a01"));
-    private static final DeviceId DEVICE_ID = new DeviceId(UUID.fromString("1d9f771a-7cdc-4ac7-838c-ba193d05a012"));
-    private static final UUID REQUEST_UUID = UUID.fromString("f64a20df-eb1e-46a3-ba6f-0b3ae053ee0a");
-
-    @Mock
-    private PartitionService partitionServiceMock;
     @Mock
     private TbClusterService tbClusterServiceMock;
 
@@ -87,48 +63,5 @@ class DefaultTbRuleEngineRpcServiceTest {
 
         // THEN
         then(tbClusterServiceMock).should().pushNotificationToCore(serviceId, restApiCallResponseMsgProto, null);
-    }
-
-    @ParameterizedTest(name = "deadlineOffset={0}, expirationOffset={1}, expectedDelay={2}")
-    @MethodSource
-    public void givenRpcRequest_whenSendRpcRequestToDevice_thenSchedulesTimeoutUsingResponseDeadline(
-            Long deadlineOffset, long expirationOffset, long expectedDelay) {
-        // GIVEN
-        ScheduledExecutorService schedulerMock = mock(ScheduledExecutorService.class);
-        ReflectionTestUtils.setField(tbRuleEngineRpcService, "scheduler", schedulerMock);
-
-        TopicPartitionInfo tpi = mock(TopicPartitionInfo.class);
-        given(tpi.isMyPartition()).willReturn(false);
-        given(partitionServiceMock.resolve(eq(ServiceType.TB_CORE), any(TenantId.class), any(DeviceId.class))).willReturn(tpi);
-
-        long now = System.currentTimeMillis();
-        var requestBuilder = RuleEngineDeviceRpcRequest.builder()
-                .tenantId(TENANT_ID)
-                .deviceId(DEVICE_ID)
-                .requestUUID(REQUEST_UUID)
-                .method("getValue")
-                .body("{}")
-                .expirationTime(now + expirationOffset);
-        if (deadlineOffset != null) {
-            requestBuilder.ruleEngineResponseDeadline(now + deadlineOffset);
-        }
-
-        // WHEN
-        tbRuleEngineRpcService.sendRpcRequestToDevice(requestBuilder.build(), response -> {});
-
-        // THEN
-        ArgumentCaptor<Long> delayCaptor = ArgumentCaptor.forClass(Long.class);
-        then(schedulerMock).should().schedule(any(Runnable.class), delayCaptor.capture(), eq(TimeUnit.MILLISECONDS));
-        // the delay can only be lower than expected by however long it took to reach scheduleTimeout
-        assertThat(delayCaptor.getValue()).isGreaterThan(expectedDelay - 500).isLessThanOrEqualTo(expectedDelay);
-    }
-
-    private static Stream<Arguments> givenRpcRequest_whenSendRpcRequestToDevice_thenSchedulesTimeoutUsingResponseDeadline() {
-        return Stream.of(
-                // deadline set explicitly: waits until the deadline, plus the one second grace period
-                Arguments.of(15_000L, 600_000L, 16_000L),
-                // deadline omitted: falls back to the expiration time, plus the one second grace period
-                Arguments.of(null, 30_000L, 31_000L)
-        );
     }
 }
